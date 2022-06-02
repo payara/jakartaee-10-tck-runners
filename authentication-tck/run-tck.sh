540d6ae86d5cb4ea9edd61b4db5976adc041297e
Gaurav Gupta <gaurav.gupta@payara.fish>
2022-06-02 23:33:04 +0530

    FISH-6006 Clean target dir

--- authentication-tck/run-tck.sh
+++ authentication-tck/run-tck.sh
@@ -3,6 +3,7 @@ if [ JAVA_HOME = "" ] ; then
     export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 fi
 
+rm -r target
 mkdir target
 wget -q https://download.eclipse.org/ee4j/jakartaee-tck/jakartaee10/staged/eftl/jakarta-authentication-tck-3.0.0-dist.zip -O target/jakarta-authentication-tck-3.0.0-dist.zip
 unzip -q target/jakarta-authentication-tck-3.0.0-dist.zip -d target
