fd7b05eca17679cacc1016f2d2265192f62ab3db
Andrew Pielage <andrew.pielage@payara.fish>
2022-07-04 10:51:31 +0100

    FISH-6342 Adjust Core Profile runner
    Signed-off-by: Andrew Pielage <andrew.pielage@payara.fish>

---
+++ core-tck/install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+BUNDLES=${SCRIPTPATH}/bundles
+
+if [ ! -d ${BUNDLES} ]; then
+   echo "bundles directory appears to be missing: panic and exit!"
+   exit 1
+fi
+
+if [ ! -f ${BUNDLES}/jakarta-core-profile-tck-10.0.0.zip ]; then
+  echo "10.0.0 TCK directory appears to be missing: panic and exit!"
+  exit 1
+fi
+
+cd ${BUNDLES}
+unzip jakarta-core-profile-tck-10.0.0.zip -d .
+
+cd core-profile-tck-10.0.0/artifacts
+mvn clean install
+
+cd ${SCRIPTPATH}
