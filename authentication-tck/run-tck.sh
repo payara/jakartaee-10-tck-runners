855400b2a330443e4f7e5db82d63b222ea730ffb
Andrew Pielage <andrew.pielage@payara.fish>
2022-08-01 13:49:04 +0100

    old-tck no longer in default module list
    Signed-off-by: Andrew Pielage <andrew.pielage@payara.fish>

--- authentication-tck/run-tck.sh
+++ authentication-tck/run-tck.sh
@@ -8,4 +8,4 @@ mkdir target
 wget -q https://download.eclipse.org/ee4j/jakartaee-tck/jakartaee10/staged/eftl/jakarta-authentication-tck-3.0.1.zip -O target/jakarta-authentication-tck-3.0.1.zip
 unzip -q target/jakarta-authentication-tck-3.0.1.zip -d target
 sed -i "/<id>custom<\/id>/r payara-profile.xml" target/authentication-tck-3.0.1/tck/pom.xml
-mvn clean install -Pcustom -pl '!old-tck-runner' -f target/authentication-tck-3.0.1/tck/pom.xml
+mvn clean install -Pcustom -f target/authentication-tck-3.0.1/tck/pom.xml
