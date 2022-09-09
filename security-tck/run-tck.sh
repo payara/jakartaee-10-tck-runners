1f70e57bb84a0a48b2c2f9c7200fc5c233574a61
Andrew Pielage <pandrex247@hotmail.com>
2022-09-09 10:33:38 +0100

    Don't skip old tck
    Signed-off-by: Andrew Pielage <pandrex247@hotmail.com>

--- security-tck/run-tck.sh
+++ security-tck/run-tck.sh
@@ -8,4 +8,4 @@ mkdir target
 wget -q https://download.eclipse.org/ee4j/jakartaee-tck/jakartaee10/staged/eftl/jakarta-security-tck-3.0.0.zip -O target/jakarta-security-tck-3.0.0.zip
 unzip -q target/jakarta-security-tck-3.0.0.zip -d target
 cp -f payara-profile.xml target/security-tck-3.0.0/tck/pom.xml
-mvn clean install -Ppayara-ci-managed -pl '!old-tck,!old-tck/build,!old-tck/run' -f target/security-tck-3.0.0/tck/pom.xml -Dglassfish.version=7.0.0-M4
+mvn clean install -Ppayara-ci-managed -f target/security-tck-3.0.0/tck/pom.xml -Dglassfish.version=7.0.0-M4
