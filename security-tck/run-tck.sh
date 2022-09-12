869592df3d119b5b86a8177a55d077f96c99d09c
Andrew Pielage <andrew.pielage@payara.fish>
2022-09-12 11:12:57 +0100

    FISH-6527 Remove glassfish.version
    Signed-off-by: Andrew Pielage <andrew.pielage@payara.fish>

--- security-tck/run-tck.sh
+++ security-tck/run-tck.sh
@@ -9,4 +9,4 @@ wget -q https://download.eclipse.org/ee4j/jakartaee-tck/jakartaee10/staged/eftl/
 unzip -q target/jakarta-security-tck-3.0.0.zip -d target
 cp -f payara-profile.xml target/security-tck-3.0.0/tck/pom.xml
 cp -f old-tck-run-payara.xml target/security-tck-3.0.0/tck/old-tck/run/pom.xml
-mvn clean install help:active-profiles -Ppayara-ci-managed -f target/security-tck-3.0.0/tck/pom.xml -Dglassfish.version=7.0.0-M8
+mvn clean install help:active-profiles -Ppayara-ci-managed -f target/security-tck-3.0.0/tck/pom.xml
