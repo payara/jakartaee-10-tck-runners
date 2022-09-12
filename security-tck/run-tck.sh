4a89aeaa3175e662e2f75190d9ba2c0a07777fb4
Andrew Pielage <andrew.pielage@payara.fish>
2022-09-12 11:08:03 +0100

    FISH-6527 Rework poms
    Signed-off-by: Andrew Pielage <andrew.pielage@payara.fish>

--- security-tck/run-tck.sh
+++ security-tck/run-tck.sh
@@ -8,5 +8,5 @@ mkdir target
 wget -q https://download.eclipse.org/ee4j/jakartaee-tck/jakartaee10/staged/eftl/jakarta-security-tck-3.0.0.zip -O target/jakarta-security-tck-3.0.0.zip
 unzip -q target/jakarta-security-tck-3.0.0.zip -d target
 cp -f payara-profile.xml target/security-tck-3.0.0/tck/pom.xml
-cp -f old-tck-payara.xml target/security-tck-3.0.0/tck/old-tck/run/pom.xml
+cp -f old-tck-run-payara.xml target/security-tck-3.0.0/tck/old-tck/run/pom.xml
 mvn clean install -Ppayara-ci-managed -f target/security-tck-3.0.0/tck/pom.xml -Dglassfish.version=7.0.0-M8
