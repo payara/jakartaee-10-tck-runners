b14f54118dde1f09d3fabd9770a21650985ca24c
Andrew Pielage <pandrex247@hotmail.com>
2022-09-15 14:33:22 +0100

    FISH-6538 First stab
    Signed-off-by: Andrew Pielage <pandrex247@hotmail.com>

--- authentication-tck/run-tck.sh
+++ authentication-tck/run-tck.sh
@@ -8,4 +8,5 @@ mkdir target
 wget -q https://download.eclipse.org/ee4j/jakartaee-tck/jakartaee10/staged/eftl/jakarta-authentication-tck-3.0.1.zip -O target/jakarta-authentication-tck-3.0.1.zip
 unzip -q target/jakarta-authentication-tck-3.0.1.zip -d target
 sed -i "/<id>custom<\/id>/r payara-profile.xml" target/authentication-tck-3.0.1/tck/pom.xml
-mvn clean install -Pcustom -f target/authentication-tck-3.0.1/tck/pom.xml
+cp old-tck-run.xml target/authentication-tck-3.0.1/tck/old-tck/run/pom.xml
+mvn clean install -Pcustom,old-tck -f target/authentication-tck-3.0.1/tck/pom.xml
