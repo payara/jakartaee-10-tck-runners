cedf307c2d65c0777cf7d9f0f10b99f764f4fa38
Andrew Pielage <andrew.pielage@payara.fish>
2022-09-27 12:14:18 +0100

    FISH-6468 Authentication web runner
    Signed-off-by: Andrew Pielage <andrew.pielage@payara.fish>

--- authentication-tck/run-tck.sh
+++ authentication-tck/run-tck.sh
@@ -10,4 +10,11 @@ wget -q https://download.eclipse.org/jakartaee/authentication/3.0/jakarta-authen
 unzip -q target/jakarta-authentication-tck-${VERSION}.zip -d target
 sed -i "/<id>custom<\/id>/r payara-profile.xml" target/authentication-tck-${VERSION}/tck/pom.xml
 cp pom-old-tck-run.xml target/authentication-tck-${VERSION}/tck/old-tck/run/pom.xml
-mvn clean verify surefire-report:report -Pcustom,old-tck,platform,\!glassfish-ci-managed -f target/authentication-tck-${VERSION}/tck/pom.xml
+
+if [[ "$PROFILE" == "web" || "$PROFILE" == "WEB" ]];then
+  mvn clean verify surefire-report:report -Pcustom,old-tck,platform,\!glassfish-ci-managed,web -f target/authentication-tck-${VERSION}/tck/pom.xml -Dpayara.artifact=payara-web
+else
+  mvn clean verify surefire-report:report -Pcustom,old-tck,platform,\!glassfish-ci-managed -f target/authentication-tck-${VERSION}/tck/pom.xml
+fi
+
+
