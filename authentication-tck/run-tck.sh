848a2586c3beb3c4a8ba8b6396c1d6e973d10cca
Andrew Pielage <andrew.pielage@payara.fish>
2022-09-27 12:15:37 +0100

    FISH-6468 reorder
    Signed-off-by: Andrew Pielage <andrew.pielage@payara.fish>

--- authentication-tck/run-tck.sh
+++ authentication-tck/run-tck.sh
@@ -12,7 +12,7 @@ sed -i "/<id>custom<\/id>/r payara-profile.xml" target/authentication-tck-${VERS
 cp pom-old-tck-run.xml target/authentication-tck-${VERSION}/tck/old-tck/run/pom.xml
 
 if [[ "$PROFILE" == "web" || "$PROFILE" == "WEB" ]];then
-  mvn clean verify surefire-report:report -Pcustom,old-tck,platform,\!glassfish-ci-managed,web -f target/authentication-tck-${VERSION}/tck/pom.xml -Dpayara.artifact=payara-web
+  mvn clean verify surefire-report:report -Pcustom,old-tck,web,\!glassfish-ci-managed -f target/authentication-tck-${VERSION}/tck/pom.xml -Dpayara.artifact=payara-web
 else
   mvn clean verify surefire-report:report -Pcustom,old-tck,platform,\!glassfish-ci-managed -f target/authentication-tck-${VERSION}/tck/pom.xml
 fi
