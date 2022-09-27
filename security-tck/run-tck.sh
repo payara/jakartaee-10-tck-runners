c639070dafcb7bf644ee6489dd2c34eb4ea32c43
Andrew Pielage <andrew.pielage@payara.fish>
2022-09-27 16:46:01 +0100

    FISH-6468 Security web runner
    Signed-off-by: Andrew Pielage <andrew.pielage@payara.fish>

--- security-tck/run-tck.sh
+++ security-tck/run-tck.sh
@@ -9,4 +9,10 @@ wget -q https://download.eclipse.org/ee4j/jakartaee-tck/jakartaee10/staged/eftl/
 unzip -q target/jakarta-security-tck-3.0.0.zip -d target
 cp -f payara-profile.xml target/security-tck-3.0.0/tck/pom.xml
 cp -f old-tck-run-payara.xml target/security-tck-3.0.0/tck/old-tck/run/pom.xml
-mvn clean install help:active-profiles -Ppayara-ci-managed -f target/security-tck-3.0.0/tck/pom.xml
+
+if [[ "$PROFILE" == "web" || "$PROFILE" == "WEB" ]];then
+  mvn clean install help:active-profiles -Ppayara-ci-managed,web -f target/security-tck-3.0.0/tck/pom.xml
+else
+  mvn clean install help:active-profiles -Ppayara-ci-managed -f target/security-tck-3.0.0/tck/pom.xml
+fi
+
