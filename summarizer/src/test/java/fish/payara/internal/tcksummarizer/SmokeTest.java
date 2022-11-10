1e659dbd18480c4dc3a0ee3a0e3e0259647a7a2d
Simon Laden <simon.laden@payara.fish>
2022-11-10 14:19:21 +0000

    FISH-6611 - summarizer for EE10 TCK runner
    Signed-off-by: Simon Laden <simon.laden@payara.fish>

---
+++ summarizer/src/test/java/fish/payara/internal/tcksummarizer/SmokeTest.java
@@ -0,0 +1,107 @@
+/*
+ *    DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ *    Copyright (c) [2019] Payara Foundation and/or its affiliates. All rights reserved.
+ *
+ *    The contents of this file are subject to the terms of either the GNU
+ *    General Public License Version 2 only ("GPL") or the Common Development
+ *    and Distribution License("CDDL") (collectively, the "License").  You
+ *    may not use this file except in compliance with the License.  You can
+ *    obtain a copy of the License at
+ *    https://github.com/payara/Payara/blob/master/LICENSE.txt
+ *    See the License for the specific
+ *    language governing permissions and limitations under the License.
+ *
+ *    When distributing the software, include this License Header Notice in each
+ *    file and include the License file at glassfish/legal/LICENSE.txt.
+ *
+ *    GPL Classpath Exception:
+ *    The Payara Foundation designates this particular file as subject to the "Classpath"
+ *    exception as provided by the Payara Foundation in the GPL Version 2 section of the License
+ *    file that accompanied this code.
+ *
+ *    Modifications:
+ *    If applicable, add the following below the License Header, with the fields
+ *    enclosed by brackets [] replaced by your own identifying information:
+ *    "Portions Copyright [year] [name of copyright owner]"
+ *
+ *    Contributor(s):
+ *    If you wish your version of this file to be governed by only the CDDL or
+ *    only the GPL Version 2, indicate your decision by adding "[Contributor]
+ *    elects to include this software in this distribution under the [CDDL or GPL
+ *    Version 2] license."  If you don't indicate a single choice of license, a
+ *    recipient has the option to distribute your version of this file under
+ *    either the CDDL, the GPL Version 2 or to extend the choice of license to
+ *    its licensees as provided above.  However, if you add GPL Version 2 code
+ *    and therefore, elected the GPL Version 2 license, then the option applies
+ *    only if the new code is made subject to such option by the copyright
+ *    holder.
+ */
+
+package fish.payara.internal.tcksummarizer;
+
+import java.io.File;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.assertTrue;
+
+public class SmokeTest {   
+
+    @Test
+    public void fileCreationTest() throws IOException, ParserConfigurationException, SAXException {
+        String[] args = new String[3];
+        //args[0]=".*[/\\\\]src[/\\\\]test[/\\\\]sample[/\\\\].*junit-report.xml";
+        args[0]="**/*junit-report.xml";
+        args[1]="jUnitReport";
+        args[2]="testSuite";
+        Main.main(args);
+        
+        
+        String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        String filename =  "summary-"+args[2]+"-"+timestamp+".txt";
+        
+        File file = new File("results/" + filename);
+        assertTrue(file.exists());
+    }   
+
+    @Test
+    public void fileCreationCustomPathTest() throws IOException, ParserConfigurationException, SAXException {
+        String[] args = new String[4];
+        args[0]="**/*tck-junit-report.xml";
+        args[1]="jUnitReport";
+        args[2]="testSuiteCustomPath";
+        args[3]="testFolder/subFolder/";
+        Main.main(args);
+        
+        
+        String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        String filename =  "summary-"+args[2]+"-"+timestamp+".txt";
+        
+        File file = new File("testFolder/subFolder/" + filename);
+        assertTrue(file.exists());
+    }
+    
+    @Test
+    public void fileCreationMultipleInput() throws IOException, ParserConfigurationException, SAXException {
+        String[] args = new String[3];
+        args[0]="**/ee.jakarta*txt";
+        args[1]="testSet";
+        args[2]="testSuiteMultipleFiles";
+        Main.main(args);
+        
+        
+        String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        String filename =  "summary-"+args[2]+"-"+timestamp+".txt";
+        
+        File file = new File("results/" + filename);
+        assertTrue(file.exists());
+    }
+}
