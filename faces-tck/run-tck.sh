eb63ad168808ee9051af3926282de6df03676095
Rhys Williams <170514543+Viii3@users.noreply.github.com>
2024-08-22 20:59:17 +0100

    Update TCK to 4.0.3

--- faces-tck/run-tck.sh
+++ faces-tck/run-tck.sh
@@ -5,18 +5,18 @@ fi
 
 rm -r target
 mkdir target
-if [ ! -f target/jakarta-faces-tck-4.0.1.zip ]; then
-    echo "Downloading jakarta-faces-tck-4.0.1.zip"
-    wget -q https://download.eclipse.org/jakartaee/faces/4.0/jakarta-faces-tck-4.0.1.zip -O target/jakarta-faces-tck-4.0.1.zip
+if [ ! -f target/jakarta-faces-tck-4.0.3.zip ]; then
+    echo "Downloading jakarta-faces-tck-4.0.3.zip"
+    wget -q https://download.eclipse.org/jakartaee/faces/4.0/jakarta-faces-tck-4.0.3.zip -O target/jakarta-faces-tck-4.0.3.zip
 fi
 echo "Unzipping"
-rm -rf target/faces-tck-4.0.1
-unzip -q target/jakarta-faces-tck-4.0.1.zip -d target
+rm -rf target/faces-tck-4.0.3
+unzip -q target/jakarta-faces-tck-4.0.3.zip -d target
 
 echo "Running TCK"
 # add payara profile, should be in the next version of upstream pom.xml
-cp pom.xml-tck target/faces-tck-4.0.1/tck/pom.xml
+cp pom.xml-tck target/faces-tck-4.0.3/tck/pom.xml
 # change usage of glassfish with payara
-cp pom.xml-old-tck-run target/faces-tck-4.0.1/tck/old-tck/run/pom.xml
-#mvn verify -P payara-ci-remote,\!glassfish-ci-managed -Dglassfish.version=6.2022.1.Alpha5-SNAPSHOT -f target/faces-tck-4.0.1/tck/pom.xml -pl old-tck -amd
-mvn verify -P payara-ci-remote,\!glassfish-ci-managed -Dglassfish.version=6.2022.1.Alpha4 -f target/faces-tck-4.0.1/tck/pom.xml
+cp pom.xml-old-tck-run target/faces-tck-4.0.3/tck/old-tck/run/pom.xml
+#mvn verify -P payara-ci-remote,\!glassfish-ci-managed -Dglassfish.version=6.2022.1.Alpha5-SNAPSHOT -f target/faces-tck-4.0.3/tck/pom.xml -pl old-tck -amd
+mvn verify -P payara-ci-remote,\!glassfish-ci-managed -Dglassfish.version=6.2022.1.Alpha4 -f target/faces-tck-4.0.3/tck/pom.xml
