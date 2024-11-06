63e00507cce201b83fb3e12fefdd53de8a4701c9
Alfonso Altamirano <alfonso.altamirano@payara.fish>
2024-11-05 18:38:03 -0600

    FISH-10056: adding property to skip setting buffer size from mojarra

--- security-tck/run-tck.sh
+++ security-tck/run-tck.sh
@@ -9,6 +9,7 @@ wget -q https://download.eclipse.org/jakartaee/security/3.0/jakarta-security-tck
 unzip -q target/jakarta-security-tck-3.0.0.zip -d target
 cp -f payara-profile.xml target/security-tck-3.0.0/tck/pom.xml
 cp -f old-tck-run-payara.xml target/security-tck-3.0.0/tck/old-tck/run/pom.xml
+cp -f web.xml target/security-tck-3.0.0/tck/old-tck/source/src/com/sun/ts/tests/securityapi/ham/customform/base/securityapi_ham_customform_base_web.xml
 
 if [[ "$PROFILE" == "web" || "$PROFILE" == "WEB" ]];then
   mvn clean install help:active-profiles -Ppayara-ci-managed,web -f target/security-tck-3.0.0/tck/pom.xml
