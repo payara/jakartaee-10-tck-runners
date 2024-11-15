9cbe10eafa7a21f379619ef354bb778365f6e741
Simon Laden <simon.laden@payara.fish>
2024-11-15 16:31:19 +0000

    FISH-9143 - fix old tck + tomcat tests

--- security-tck/run-tck.sh
+++ security-tck/run-tck.sh
@@ -7,8 +7,9 @@ rm -r target
 mkdir target
 wget -q https://download.eclipse.org/jakartaee/security/4.0/jakarta-security-tck-4.0.0.zip -O target/jakarta-security-tck-4.0.0.zip
 unzip -q target/jakarta-security-tck-4.0.0.zip -d target
+sed -i 's/\${glassfish.root}\/glassfish8/\${payara.root}\/payara7/g' target/security-tck-4.0.0/tck/**/pom.xml
 cp -f payara-profile.xml target/security-tck-4.0.0/tck/pom.xml
-#cp -f old-tck-run-payara.xml target/security-tck-4.0.0/tck/old-tck/run/pom.xml
+cp -f old-tck-run-payara.xml target/security-tck-4.0.0/tck/old-tck/run/pom.xml
 
 if [[ "$PROFILE" == "web" || "$PROFILE" == "WEB" ]];then
   mvn clean install help:active-profiles -Ppayara-ci-managed,web -f target/security-tck-4.0.0/tck/pom.xml
