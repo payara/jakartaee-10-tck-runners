80d69c04781c5bb2ccec97cbaee03651b565c1d3
Petr Aubrecht <aubrecht@asoftware.cz>
2024-11-20 10:04:20 +0100

    FISH-9608 update faces runner to tck 4.1.1

--- faces-tck/run-tck.sh
+++ faces-tck/run-tck.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 if [ JAVA_HOME = "" ] ; then
-    export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+    export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
 fi
 
-rm -r target
-mkdir target
-if [ ! -f target/jakarta-faces-tck-4.0.1.zip ]; then
-    echo "Downloading jakarta-faces-tck-4.0.1.zip"
-    wget -q https://download.eclipse.org/jakartaee/faces/4.0/jakarta-faces-tck-4.0.1.zip -O target/jakarta-faces-tck-4.0.1.zip
-fi
-echo "Unzipping"
-rm -rf target/faces-tck-4.0.1
-unzip -q target/jakarta-faces-tck-4.0.1.zip -d target
-
-echo "Running TCK"
-# add payara profile, should be in the next version of upstream pom.xml
-cp pom.xml-tck target/faces-tck-4.0.1/tck/pom.xml
-# change usage of glassfish with payara
-cp pom.xml-old-tck-run target/faces-tck-4.0.1/tck/old-tck/run/pom.xml
-#mvn verify -P payara-ci-remote,\!glassfish-ci-managed -Dglassfish.version=6.2022.1.Alpha5-SNAPSHOT -f target/faces-tck-4.0.1/tck/pom.xml -pl old-tck -amd
-mvn verify -P payara-ci-remote,\!glassfish-ci-managed -Dglassfish.version= -f target/faces-tck-4.0.1/tck/pom.xml
+mvn clean verify
