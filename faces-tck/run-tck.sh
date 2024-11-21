1fb785945406f79abb1ac150d744f514498dbe62
Petr Aubrecht <aubrecht@asoftware.cz>
2024-11-21 12:44:37 +0100

    FISH-9608 add download to faces script for jenkins

--- faces-tck/run-tck.sh
+++ faces-tck/run-tck.sh
@@ -3,4 +3,9 @@ if [ JAVA_HOME = "" ] ; then
     export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
 fi
 
+echo "Downloading Faces TCK"
+cd ..
+mvn clean install -pl .,:tck-download,:jakarta-faces-tck
+cd faces-tck
+
 mvn clean verify
