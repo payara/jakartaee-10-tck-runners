ce7195dc129f400dfe1ff3a31be9f3d84e045c0c
luiseufrasio <luis.eufrasio@gmail.com>
2025-03-03 09:36:21 -0300

    FISH-10181 : chaging refs to .jks to .p12

--- security-tck/run-tck.sh
+++ security-tck/run-tck.sh
@@ -8,6 +8,7 @@ mkdir target
 wget -q https://download.eclipse.org/jakartaee/security/4.0/jakarta-security-tck-4.0.0.zip -O target/jakarta-security-tck-4.0.0.zip
 unzip -q target/jakarta-security-tck-4.0.0.zip -d target
 sed -i 's/\${glassfish.root}\/glassfish8/\${payara.root}\/payara7/g' target/security-tck-4.0.0/tck/**/pom.xml
+sed -i 's/cacerts.jks/cacerts.p12/g' target/security-tck-4.0.0/tck/**/pom.xml
 cp -f payara-profile.xml target/security-tck-4.0.0/tck/pom.xml
 cp -f old-tck-run-payara.xml target/security-tck-4.0.0/tck/old-tck/run/pom.xml
 
