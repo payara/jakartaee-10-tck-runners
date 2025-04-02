0581771343f0cd6aea6d3cbaa320428d9fa3f220
luiseufrasio <luis.eufrasio@gmail.com>
2025-04-02 17:33:57 -0300

    FISH-9163 : solving ${payara.home}/bin/asadmin: bad substitution

--- cdi-platform-tck/verify_and_start_domain.sh
+++ cdi-platform-tck/verify_and_start_domain.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-STATUS=$(${payara.home}/bin/asadmin list-domains | grep "domain1 running")
+
+PAYARA_HOME=$1
+STATUS=$($PAYARA_HOME/bin/asadmin list-domains | grep "domain1 running")
 
 if [ -z "$STATUS" ]; then
     echo "Domain1 is not running. Starting domain1..."
-    ${payara.home}/bin/asadmin start-domain domain1
+    $PAYARA_HOME/bin/asadmin start-domain domain1
 else
     echo "Domain1 is already running."
 fi
