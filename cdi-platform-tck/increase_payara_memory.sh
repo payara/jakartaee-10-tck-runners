b029e335a5dfcf52b45191b2d49027548e46de55
luiseufrasio <luis.eufrasio@gmail.com>
2025-04-02 18:37:53 -0300

    FISH-9163 : removing regex

--- cdi-platform-tck/increase_payara_memory.sh
+++ cdi-platform-tck/increase_payara_memory.sh
@@ -12,16 +12,8 @@ else
     echo "Domain $DOMAIN_NAME is already running."
 fi
 
-EXISTING_OPTIONS=$($PAYARA_HOME/bin/asadmin list-jvm-options | grep -E '-Xmx[0-9]+m')
-if [ -n "$EXISTING_OPTIONS" ]; then
-    echo "Existing JVM options found: $EXISTING_OPTIONS. Deleting..."
-    for OPTION in $EXISTING_OPTIONS; do
-        $PAYARA_HOME/bin/asadmin delete-jvm-options "$OPTION"
-    done
-fi
+$PAYARA_HOME/bin/asadmin delete-jvm-options "-Xmx512m":"-Xmx1024m":"-Xmx2048m"
 
-echo "Adding new JVM option: $NEW_JVM_OPTION..."
 $PAYARA_HOME/bin/asadmin create-jvm-options "$NEW_JVM_OPTION"
 
-echo "Stopping $DOMAIN_NAME..."
 $PAYARA_HOME/bin/asadmin stop-domain $DOMAIN_NAME
